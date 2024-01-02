d564b812c3fc71062e4f9067dcd1742624c7ffc8
Md Josim <josimhossain842@gmail.com>
2024-01-02 11:46:09 +0600

    Make bold, italic and underscore fetures

---
+++ main.js
@@ -0,0 +1,40 @@
+const bold_btn = document.getElementById('bold_btn')
+const italic_btn = document.getElementById('italic_btn')
+const underline_btn = document.getElementById('underline_btn')
+const text_left= document.getElementById('text_left')
+const text_center = document.getElementById('text_center')
+const text_right = document.getElementById('text_right')
+const text_default = document.getElementById('text_default')
+const clipboard = document.getElementById('clipboard')
+const text_box = document.getElementById('text_box')
+const color_picker = document.getElementById('color_picker')
+
+
+bold_btn.addEventListener('click', function(){
+   if(text_box.value == ''){
+    alert("Write something!")
+   }else{
+    text_box.classList.toggle('bold');
+    bold_btn.classList.toggle('clicked_btn')
+   }
+})
+
+italic_btn.addEventListener('click', function(){
+   if(text_box.value == ''){
+    alert("Write something!")
+   }else{
+    text_box.classList.toggle('italic');
+    italic_btn.classList.toggle('clicked_btn')
+   }
+})
+
+underline_btn.addEventListener('click', function(){
+   if(text_box.value == ''){
+    alert("Write something!")
+   }else{
+    text_box.classList.toggle('underline');
+    underline_btn.classList.toggle('clicked_btn')
+   }
+})
+
+
